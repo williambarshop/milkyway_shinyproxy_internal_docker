9942c9f83767bf459d3873873e8acadce525bec4
William Barshop <wbarshop@ucla.edu>
2019-02-13 22:52:00 -0800

    Build images to CI and not dev

--- build_docker_image.sh
+++ build_docker_image.sh
@@ -7,5 +7,5 @@ sed -i "s/commit_rev/$now/g" build_dir/Dockerfile ;
 sed -i "s/CI_job_ID/$now/g" build_dir/Dockerfile ;
 sed -i "s/DATE-REPLACE/$now/g" build_dir/Dockerfile ;
 
-sudo docker build -t wbarshop/milkyway_shiny:dev build_dir/ --no-cache=false ;
+sudo docker build -t wbarshop/milkyway_shiny:ci build_dir/ --no-cache=false ;
 rm -rf build_dir
